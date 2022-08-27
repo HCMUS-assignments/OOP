839e9072274b33fb36f09f44535b7d79dc35a602
thanhngan22 <nganbui9a2017@gmail.com>
2022-08-27 23:42:07 +0700

    solved: done ex07 :orange_heart:

--- solutions/Lab 2/Ex07.java
+++ solutions/Lab 2/Ex07.java
@@ -1,28 +1,26 @@
-// write a function public static int findMax(int arr[]) to find the maximum value in an array.
+// write a func to square all the elements of an array
 
 import java.util.Scanner;
 
-public class Ex01 {
-    public static int findMax(int arr[]) {
-            int max = arr[0];
-            for (int value : arr) {
-                if (value > max) {
-                    max = value;
-                }
-            }
-            return max;
+public class Ex07 {
+    public static void squareAll(int arr[]) {
+        for (int i=0; i<arr.length; i++) {
+            arr[i] = arr[i] * arr[i];
+        }
     }
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of elements in the array: ");
+        System.out.println("Enter the size of the array: ");
         int n = sc.nextInt();
         int arr[] = new int[n];
         System.out.println("Enter the elements of the array: ");
         for (int i=0; i<n; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println("The maximum value in the array is: " + findMax(arr));
+        squareAll(arr);
+        for (int i=0; i<n; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
