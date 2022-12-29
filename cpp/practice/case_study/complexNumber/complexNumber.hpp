c7a65160da95d6c029eb124939efb7393bcfead8
thanhngan22 <nganbui9a2017@gmail.com>
2022-12-29 11:48:47 +0700

    :cherries: add case study of book/ init complex number class

---
+++ cpp/practice/case_study/complexNumber/complexNumber.hpp
@@ -0,0 +1,48 @@
+// Đồ án học tập: 
+// Lớp số phức: ComplexNumber
+// Bài toán: xây dựng lớp số phức có thể thực hiện các phép toán số học thông qua toán tử: + - * / == !=
+
+#include <iostream>
+using namespace std;
+
+class ComplexNumber {
+    private:
+        double r;   // real part
+        double i;   // imaginary part
+    public:
+        // constructor
+        ComplexNumber(double r = 0, double i = 0);
+        ComplexNumber(const ComplexNumber &c);
+        
+        // destructor
+        ~ComplexNumber();
+
+        // methods
+
+        void setReal(double r);
+
+        void setImaginary(double i);
+
+        double getReal() const;
+
+        double getImaginary() const;
+
+        // operator overloading
+        ComplexNumber operator + (const ComplexNumber &c) const;
+
+        ComplexNumber operator - (const ComplexNumber &c) const;
+
+        ComplexNumber operator * (const ComplexNumber &c) const;
+
+        ComplexNumber operator / (const ComplexNumber &c) const;
+
+        bool operator == (const ComplexNumber &c) const;
+
+        bool operator != (const ComplexNumber &c) const;
+
+        // friend function
+        friend ostream &operator << (ostream &out, const ComplexNumber &c); 
+
+        friend istream &operator >> (istream &in, ComplexNumber &c);
+
+};
