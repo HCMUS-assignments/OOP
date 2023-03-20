2b21c0c11ca457fddfa7431735583d0a70295aeb
thanhngan22 <nganbui9a2017@gmail.com>
2023-03-21 01:43:18 +0700

    init deadline :))

---
+++ moodle/LT/LT02/Bai06.cpp
@@ -0,0 +1,6 @@
+/*
+Tập tin INPUT.txt chứa số lượng tin nhắn bình chọn của khán giả do ca sĩ mình yêu thích.
+"Mã bình chọn, ca sĩ được bình chọn, thời gian bình chọn."
+Hãy in ra "OUTPUT.txt" ca sĩ được bình chọn nhiều nhất.
+*** Lưu ý: Dòng đầu tiên là giờ mở tổng đài bình chọn và đóng tổng đài bình chọn.
+*/
---
+++ moodle/LT/LT02/Bai07.cpp
@@ -0,0 +1,5 @@
+/*
+Tập tin "INPUT.txt". Cho danh sách các thiên thạch hình tròn gồm {Mã, Tọa độ tâm, bán kính, tốc độ bay}
+Giả sử trái đất nằm ở tọa độ (0, 0), hãy tính xem thiên thạch nào sẽ va chạm trái đất đầu tiên.
+Xuất ra mã số và thời gian sẽ va chạm.
+*/
