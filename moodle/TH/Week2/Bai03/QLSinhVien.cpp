a4d70970ad2df67b02e5bcc1cd39a9644bcc1f8e
thanhngan22 <nganbui9a2017@gmail.com>
2023-03-20 17:41:11 +0700

    define constructors, destructor and methods of class QLSinhVien

--- moodle/TH/Week2/Bai03/QLSinhVien.cpp
+++ moodle/TH/Week2/Bai03/QLSinhVien.cpp
@@ -1,48 +1,91 @@
 #include "QLSinhVien.h"
 
-    // default constructor
-    QLSinhVien::QLSinhVien(){
-        
-    }
+// default constructor
+QLSinhVien::QLSinhVien()
+{
+    size = 0;
+    avgMarkClass = 0;
 
-    // destructor
-    QLSinhVien::~QLSinhVien(){
-        
-    }
+    head = NULL;
+}
 
-    // methods
-
-    // 3.  write list of students in file
-    void QLSinhVien::write(){
-        
+// destructor
+QLSinhVien::~QLSinhVien()
+{
+    if (head != NULL)
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            temp = head->next;
+            delete head;
+            head = temp;
+        }
     }
+}
 
-    // 4. read list of students from file
-    void QLSinhVien::read(){
-        
-    }
+// methods
 
-    // 5. find the students having the average score less than class's one, then write that list in file
-    void QLSinhVien::find(){
-        
-    }
+// 3.  write list of students in file
+void QLSinhVien::write()
+{
 
-    // 6. add a new student in list and update file
-    void QLSinhVien::add(SinhVien &){
-        
-    }
+}
 
-    // 7. classify the students
-    void QLSinhVien::classify(){
-        
-    }
+// 4. read list of students from file
+void QLSinhVien::read()
+{
+}
 
-    // 8. output list of students with classification inf
-    void QLSinhVien::output_classification(){
-        
-    }
+// 5. find the students having the average score less than class's one, then write that list in file
+void QLSinhVien::find()
+{
+}
 
-    // 9. find all students having the same birthday with the present day
-    void QLSinhVien::findSameBirthday(){
-        
+// 6. add a new student in list and update file
+void QLSinhVien::add()
+{
+    SinhVien sv;
+    cin >> sv;
+    Node *pNode = new Node;
+    pNode->data = sv;
+    pNode->next = NULL;
+
+    if (head == NULL)
+    {
+        head = pNode;
     }
+    else
+    {
+        Node *temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = pNode;
+    }
+    size++;
+
+    // update file
+    fout.open("DSSV.txt", ios::app); // ios::app move to the end of file
+    if (fout.fail()) {
+        cout << "Can't open file DSSV.txt\n";
+        return;
+    }
+    fout << sv;
+    fout.close();
+}
+
+// 7. classify the students
+void QLSinhVien::classify()
+{
+}
+
+// 8. output list of students with classification inf
+void QLSinhVien::output_classification()
+{
+}
+
+// 9. find all students having the same birthday with the present day
+void QLSinhVien::findSameBirthday()
+{
+}
--- moodle/TH/Week2/Bai03/QLSinhVien.h
+++ moodle/TH/Week2/Bai03/QLSinhVien.h
@@ -8,14 +8,17 @@ struct Node
     SinhVien data;
     Node *next;
 
-    float avgMarkClass;
 };
 
 class QLSinhVien
 {
 private:
     Node *head;
-    int count;
+    int size;
+    float avgMarkClass;
+
+    ifstream fin;
+    ofstream fout;
 
 public:
     // default constructor
@@ -26,25 +29,27 @@ public:
 
     // methods
 
-    // 3.  write list of students in file
+    // 3.  write list of students in file: viết hàm ghi danh sách sinh viên lên tập tin
     void write();
 
-    // 4. read list of students from file
+    // 4. read list of students from file: đọc danh sách sinh viên từ tập tin
     void read();
 
     // 5. find the students having the average score less than class's one, then write that list in file
+    //      : tìm các sinh viên có đtb nhỏ hơn đtb của cả lớp, ghi dssv vào tập tin
     void find();
 
-    // 6. add a new student in list and update file
-    void add(SinhVien &);
+    // 6. add a new student in list and update file: thêm sv mới vào ds, cập nhật tập tin
+    void add();
 
-    // 7. classify the students
+    // 7. classify the students: xếp loại sinh viên
     void classify();
 
-    // 8. output list of students with classification inf
+    // 8. output list of students with classification inf: xuất thông tin sinh viên có thông tin xếp loại
     void output_classification();
 
     // 9. find all students having the same birthday with the present day
+    //      : tìm tất cả sv có cùng ngày sinh trong ngày hiện tại
     void findSameBirthday();
 
 };
