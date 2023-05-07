5fd16ef298ed6f7bd5c3fcc2c3c0dcb3d038bd30
thanhngan22 <nganbui9a2017@gmail.com>
2023-05-07 13:58:14 +0700

    finished tasks of string | lab 5

--- java/solutions/Lab5/_String.java
+++ java/solutions/Lab5/_String.java
@@ -41,12 +41,48 @@ public class _String {
         return new String(chars);
     }
 
+    // part 2
+    public static int countWord(String paragraph) {
+        // count the number of words
+        String temp = new String(paragraph);
+        temp.replaceAll("[,.]", "");
+        String[] words = temp.split(" ");
+        return words.length;
+    }
+
+    public static int countSentences(String paragraph) {
+        // count the number of sentences
+        String[] sentences = paragraph.split("\\.");
+        return sentences.length;
+
+    }
+
+    public static int countAppear(String paragraph, String word) {
+        // count the number of occurrences of the word in the paragraph
+        String temp = new String(paragraph);
+        temp.replaceAll("[,.]", "");
+        String[] words = temp.split(" ");
+
+        Map<String, Integer> counts = new HashMap<String, Integer>();
+        for (int i = 0; i < words.length; i++) {
+            counts.compute(words[i], (k,v) -> v == null ? 1 : v + 1);
+        }
+        return counts.get(word);
+    }
+
+
     public static void main(String [] args) {
         String str = "Nguyen Le Trong Tin";
         System.out.println("Shortname: " + shortName(str));
         System.out.println("hastagName: " + hashtagName(str));
         System.out.println("upper case all vowel: " + upperCaseAllVowel(str));
         System.out.println("upper case all N: " + upperCaseAllN(str));
+
+        System.out.println("Part 2: ");
+        String paragraph = "The Edge Surf is of course also a whole lot better, which will hopefully win Microsoft some converts. It offers time trial, support for other input methods like touch and gamepads, accessibility improvements, high scores, and remastered visuals.";
+        System.out.println("The number of words : " + countWord(paragraph));
+        System.out.println("The number of sentences : " + countSentences(paragraph));
+        System.out.println("The number of occurrences \"and\" : " + countAppear(paragraph, "and"));
 
     }
 }
